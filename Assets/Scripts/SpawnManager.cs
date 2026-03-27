45301764b93d94899b1ca93d5b88ecfe9e0a1040
punodsep <poundpunnapha@gmail.com>
2026-03-27 16:47:39 +0700

    wip in class

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -7,7 +8,39 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
+        StartCoroutine(SpawnRoutine());
+        //InvokeRepeating(nameof(RandomSpawn), 0, 5);
 
+        //StartCoroutine(Hello());
+        //StartCoroutine(GoodBye());
     }
 
+    IEnumerator SpawnRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    void RandomSpawn()
+    {
+        var index = Random.Range(0, spawnPoints.Length);
+        var spawnPoint = spawnPoints[index];
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+    }
+
+    /*IEnumerator GoodBye()
+    { 
+        yield return new WaitForSeconds(1);
+        Debug.Log("Bye " + Time.frameCount + " " + Time.time);
+    }
+
+    IEnumerator Hello()
+    {
+        Debug.Log("Hello " + Time.frameCount);
+        yield return null;
+    }*/
 }
